dbf6897568dc16c8713d9e2882187637934e1e8c
Mostafa Magdy <mostafa_magdy@fci.helwan.edu.eg>
2019-12-05 16:20:14 +0200

    add functions for control reservations , with validations for reservation Time

--- api/controllers/reservationContoller.js
+++ api/controllers/reservationContoller.js
@@ -1,18 +1,27 @@
-var reservation = require('../models/reservation');
+var reservationModel = require('../models/reservation');
 
 
 userId="5de6a0cc0531d6627c420e02";
 
-
 exports.index = async function(req, res) {
-    const reservations = await reservation.find({userId:userId}).select("-__v").populate('userId').populate('carId').exec();
+    const reservations = await reservationModel.find({userId:userId}).select("-__v").populate('userId').populate('carId').exec();
     return res.status(200).json({data : reservations, message : null , errors : null});
 };
 
 exports.store = async function(req, res) {
-    const newReservation = new reservation({
-        startTime: req.body.startTime,
-        endTime: req.body.endTime,
+    //update time to timezone "cairo"
+    startTime=updateTime(req.body.startTime);
+    endTime=updateTime(req.body.endTime);
+    //cehck valida reservations
+    const reservations = await reservationModel.find({carId:"5de6a0cc0531d6627c420e01"}).exec();
+    errs=checkReservations(startTime,endTime,"5de6a0cc0531d6627c420e01",reservations);    
+    if(errs){
+        return res.status(422).json({data : req.body , message : "Invalid Time" , errs : errs});
+    }
+    //make reservation
+    const newReservation = new reservationModel({
+        startTime: startTime,
+        endTime: endTime,
         userId : userId,
         carId:"5de6a0cc0531d6627c420e01"
       });
@@ -21,15 +30,67 @@ exports.store = async function(req, res) {
 };
 
 exports.show = async function(req, res) {
-    const reservations = await reservation.find({_id:req.params.id}).select("-__v").populate('userId').populate('carId').exec();
+    const reservations = await reservationModel.find({_id:req.params.id}).select("-__v").populate('userId').populate('carId').exec();
     return res.status(200).json({data : reservations , message : null , errors : null});
 };
 
-exports.update = function(req, res) {
-    res.send('NOT IMPLEMENTED: Site Home Page');
+exports.update = async function(req, res) {
+    //update time to timezone "cairo"
+    startTime=updateTime(req.body.startTime);
+    endTime=updateTime(req.body.endTime);
+    //cehck valida reservations
+    const reservations = await reservationModel.find({carId:"5de6a0cc0531d6627c420e01"}).exec();
+    errs=checkReservations(startTime,endTime,"5de6a0cc0531d6627c420e01",reservations);    
+    if(errs){
+        return res.status(422).json({data : req.body , message : "Invalid Time" , errs : errs});
+    }
+    const reservation = await reservationModel.updateOne({
+        _id: req.params.id
+      }, {
+        $set: {
+        startTime: startTime,
+        endTime: endTime,
+        }
+      }).exec(); 
+    return res.status(200).json({data : reservation , message : null , errors : null});
 };
 
 exports.delete = async function(req, res) {
-    const reservations = await reservation.deleteMany({_id:req.params.id}).exec();
+    const reservations = await reservationModel.deleteMany({_id:req.params.id}).exec();
     return res.status(200).json({data : reservations , message : null , errors : null});
 };
+
+function checkReservations(startTime , endTime , carId,reservations){
+    var errors={};
+    console.log(startTime);
+    console.log(endTime);
+    for (const reservation of reservations) {
+        if(reservation.carId == carId){
+            if(startTime.getTime() >= reservation.startTime.getTime()  && startTime.getTime()  <= reservation.endTime.getTime() ){
+                errors.startTime="car was reserved from "+reservation.startTime.toLocaleString('en-IN')+" to "+reservation.endTime.toLocaleString('en-IN') +", plz choose another startTime";
+            }
+            if(endTime.getTime()  >= reservation.startTime.getTime()  && endTime.getTime()  <= reservation.endTime.getTime() ){
+                errors.endTime="car was reserved from "+reservation.startTime.toLocaleString('en-IN')+" to "+reservation.endTime.toLocaleString('en-IN') +", plz choose another startTime";
+            }
+            if(reservation.startTime.getTime()  >= startTime.getTime()  && reservation.startTime.getTime()  <= endTime.getTime() ){
+                errors.startTime="car was reserved from "+reservation.startTime.toLocaleString('en-IN')+" to "+reservation.endTime.toLocaleString('en-IN') +", plz choose another startTime";
+            }
+            if(reservation.endTime.getTime()  >= startTime.getTime()  && reservation.endTime.getTime()  <= endTime.getTime() ){
+                errors.startTime="car was reserved from "+reservation.startTime.toLocaleString('en-IN')+" to "+reservation.endTime.toLocaleString('en-IN') +", plz choose another startTime";
+            }
+      }
+    }
+    if(isEmpty(errors)) return null;
+    return errors;
+}
+
+function isEmpty(obj) {
+    return Object.getOwnPropertyNames(obj).length === 0;
+}
+
+function updateTime(time){
+    newTime=new Date(time);
+    newTime.setSeconds(0,0);
+    newTime.setHours( newTime.getHours() + 2 );
+    return newTime;
+}
