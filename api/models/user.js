ce73e8645f90c11cefdefb6743e63b5656417288
Mostafa Magdy <mostafa_magdy@fci.helwan.edu.eg>
2019-12-08 21:39:17 +0200

    remove reuired from some attributes

--- api/models/user.js
+++ api/models/user.js
@@ -7,9 +7,9 @@ email: {type: String , required : true},
 username: {type: String , required : true},
 password: {type: String , required : true},
 phone: {type: String , required : true},
-address: {type: String , required : true},
-photo: {type: String , required : true},
-userType: {type: Number},
+address: {type: String , required : false},
+photo: {type: String , required : false},
+userType: {type: Number, required : false},
 });
 
 schema.methods.generateAuthToken = function () {
