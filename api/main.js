ac53be7aabbb5f4dbed4d834058e680aa3fbd6db
Muhammad Tarek <muhammadtarek96@gmail.com>
2019-12-13 08:56:24 +0200

    fix: bad merge

--- api/main.js
+++ api/main.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const hostname = '127.0.0.1';
 const port = process.env.port || 3006;
@@ -31,11 +32,11 @@ const carRouter = require('./routes/carRoute');
 
 app.use(cors())
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({
-  extended: false
+  limit: "50mb",
+  extended: true
 }))
-
 
 app.use('/api/auth',authRouter);
 app.use('/api/user',userRouter);
